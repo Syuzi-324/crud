24a3f6d2649a3adde20b2541e7ceb3d05d7f689e
Syuzanna Simonyan <syuzanna.simonyan@wic.local>
2021-03-11 17:23:59 +0400

    third commit

---
+++ controllers/AdminController.js
@@ -0,0 +1,41 @@
+const { Article }=require("../models/ArticleModel")
+
+class AdminController{
+    AdminView(req,res){
+        res.render("admin")
+    }
+
+    async getAllArticles(req,res){
+        let Articles=await Article.find()
+        res.render("adminArticle",{articles:Articles})
+
+    }
+
+    createArticleView(req,res){
+        res.render("ArticleCreator")
+    }
+    async addNewArticle(req,res){
+        console.log("file",req.file)
+        let newArticle=new Article({
+            title:req.body.title,
+            description:req.body.description,
+            content:req.body.content,
+            image:req.file.filename
+        })
+
+        let savedArticle=await newArticle.save()
+        res.redirect("/admin/articles")
+
+
+    }
+    async deleteArticle(req,res){
+  
+        let Articles=await Article.deleteOne({_id:req.params.id})
+        res.redirect("/admin/articles")
+
+    }
+
+
+}
+
+module.exports=new AdminController()
---
+++ routes/AdminRouter.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path=require("path")
+const multer=require("multer")
+const { AdminView, getAllArticles,createArticleView,  addNewArticle,deleteArticle} =require("../controllers/AdminController")
+const router = express.Router();
+
+
+const Storage=multer.diskStorage({
+    destination:function(req,file, cb){
+        cb(null,path.join(__dirname,"..","/public/images") )
+    },
+    filename:function(req,file, cb){
+        cb(null,Date.now()+file.originalname) 
+    }
+})
+
+
+const upload=multer({
+    storage:Storage,
+    limit:4*1024*1024,
+  
+})
+/* admin view */
+router.get('/',AdminView);
+
+router.get('/articles',getAllArticles);
+
+router.get("/articles/articleCreate", createArticleView)
+
+router.post("/articles/articleCreate",upload.single("articleImg"), addNewArticle)
+
+router.get("/articles/delete/:id", deleteArticle)
+
+
+module.exports = router;
